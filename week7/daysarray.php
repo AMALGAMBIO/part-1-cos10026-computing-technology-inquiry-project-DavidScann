66af3ae4ef3d33bc5bfc774f7f44cdb746473e58
DavidScann <taviousandkev@gmail.com>
2025-03-26 14:00:05 +0700

    task: completed task 2 (reassigning variable practise)

--- week7/daysarray.php
+++ week7/daysarray.php
@@ -6,10 +6,32 @@
 <body>
     <?php
     $days = array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    $lastElement = end($days);
+    $lastElementEng = end($days);
     echo "The Days of the week in English are: ";
     foreach ($days as $day) {
-        if ($day == $lastElement) {
+        if ($day == $lastElementEng) {
+            echo "$day.";
+        } else {
+            echo "$day, ";
+        }
+    }
+
+    echo "<br><br>The Days of the week in French are: ";
+
+    // Reassigning the values of the array
+    $days[0] = "Dimanche";
+    $days[1] = "Lundi";
+    $days[2] = "Mardi";
+    $days[3] = "Mercredi";
+    $days[4] = "Jeudi";
+    $days[5] = "Vendredi";
+    $days[6] = "Samedi";
+    
+    $lastElementFr = end($days);
+
+    echo "The Days of the week in French are: ";
+    foreach ($days as $day) {
+        if ($day == $lastElementFr) {
             echo "$day.";
         } else {
             echo "$day, ";
